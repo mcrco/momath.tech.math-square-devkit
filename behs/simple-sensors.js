e99fc94d0acff4a1a08014521afd581168f8e514
Guillermo <acevedo@momath.org>
2026-07-24 11:01:57 -0400

    fix(behs): restore canvas width/height 100% for correct mouse mapping and fit

--- behs/simple-sensors.js
+++ behs/simple-sensors.js
@@ -21,9 +21,8 @@ function init(container) {
   canvas = document.createElement('canvas');
   canvas.width = Display.width;
   canvas.height = Display.height;
-  canvas.style.position= 'left';
-  //canvas.style.width = '100%';
-  //canvas.style.height = '100%';
+  canvas.style.width = '100%';
+  canvas.style.height = '100%';
   canvas.style.pointerEvents = 'none';
   container.appendChild(canvas);
   ctx = canvas.getContext('2d');
